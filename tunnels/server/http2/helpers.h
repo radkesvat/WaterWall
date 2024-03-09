5c799be801b6c5bfa2f635dcf3ccc09089464103
radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-03-09 14:46:07 +0000

    progress

---
+++ tunnels/server/http2/helpers.h
@@ -0,0 +1,26 @@
+#pragmo onec
+
+static nghttp2_nv make_nv(const char* name, const char* value) {
+    nghttp2_nv nv;
+    nv.name = (uint8_t*)name;
+    nv.value = (uint8_t*)value;
+    nv.namelen = strlen(name);
+    nv.valuelen = strlen(value);
+    nv.flags = NGHTTP2_NV_FLAG_NONE;
+    return nv;
+}
+
+static nghttp2_nv make_nv2(const char* name, const char* value,
+        int namelen, int valuelen) {
+    nghttp2_nv nv;
+    nv.name = (uint8_t*)name;
+    nv.value = (uint8_t*)value;
+    nv.namelen = namelen; nv.valuelen = valuelen;
+    nv.flags = NGHTTP2_NV_FLAG_NONE;
+    return nv;
+}
+
+static void print_frame_hd(const nghttp2_frame_hd* hd) {
+    printd("[frame] length=%d type=%x flags=%x stream_id=%d\n",
+        (int)hd->length, (int)hd->type, (int)hd->flags, hd->stream_id);
+}
--- tunnels/server/http2/http2_server.c
+++ tunnels/server/http2/http2_server.c
@@ -43,8 +43,147 @@ typedef struct http2_server_con_state_s
 
     nghttp2_session *session;
     http2_session_state state;
+    int error;
+    int stream_id;
+    int stream_closed;
+    int frame_type_when_stream_closed;
+    // http2_frame_hd + grpc_message_hd
+    // at least HTTP2_FRAME_HDLEN + GRPC_MESSAGE_HDLEN = 9 + 5 = 14
+    unsigned char frame_hdbuf[32];
 
 } http2_server_con_state_t;
+
+
+
+
+static int on_header_callback(nghttp2_session *session,
+                              const nghttp2_frame *frame,
+                              const uint8_t *_name, size_t namelen,
+                              const uint8_t *_value, size_t valuelen,
+                              uint8_t flags, void *userdata)
+{
+    printd("on_header_callback\n");
+    print_frame_hd(&frame->hd);
+    const char *name = (const char *)_name;
+    const char *value = (const char *)_value;
+    printd("%s: %s\n", name, value);
+    if (*name == ':')
+    {
+        if (strcmp(name, ":method") == 0)
+        {
+            // req->method = http_method_enum(value);
+        }
+        else if (strcmp(name, ":path") == 0)
+        {
+            // req->url = value;
+        }
+        else if (strcmp(name, ":scheme") == 0)
+        {
+            // req->headers["Scheme"] = value;
+        }
+        else if (strcmp(name, ":authority") == 0)
+        {
+            // req->headers["Host"] = value;
+        }
+    }
+    else
+    {
+        // hp->parsed->headers[name] = value;
+        if (strcmp(name, "content-type") == 0)
+        {
+            // hp->parsed->content_type = http_content_type_enum(value);
+        }
+    }
+    
+    return 0;
+    
+}
+
+static int on_data_chunk_recv_callback(nghttp2_session *session,
+                                       uint8_t flags, int32_t stream_id, const uint8_t *data,
+                                       size_t len, void *userdata)
+{
+    printd("on_data_chunk_recv_callback\n");
+    printd("stream_id=%d length=%d\n", stream_id, (int)len);
+    // printd("%.*s\n", (int)len, data);
+    Http2Parser *hp = (Http2Parser *)userdata;
+
+    if (hp->parsed->ContentType() == APPLICATION_GRPC)
+    {
+        // grpc_message_hd
+        if (len >= GRPC_MESSAGE_HDLEN)
+        {
+            grpc_message_hd msghd;
+            grpc_message_hd_unpack(&msghd, data);
+            printd("grpc_message_hd: flags=%d length=%d\n", msghd.flags, msghd.length);
+            data += GRPC_MESSAGE_HDLEN;
+            len -= GRPC_MESSAGE_HDLEN;
+            // printd("%.*s\n", (int)len, data);
+        }
+    }
+    if (hp->parsed->http_cb)
+    {
+        hp->parsed->http_cb(hp->parsed, HP_BODY, (const char *)data, len);
+    }
+    else
+    {
+        hp->parsed->body.append((const char *)data, len);
+    }
+    return 0;
+}
+
+static int on_frame_recv_callback(nghttp2_session *session,
+                                  const nghttp2_frame *frame, void *userdata)
+{
+    printd("on_frame_recv_callback\n");
+    print_frame_hd(&frame->hd);
+    Http2Parser *hp = (Http2Parser *)userdata;
+    switch (frame->hd.type)
+    {
+    case NGHTTP2_DATA:
+        hp->state = H2_RECV_DATA;
+        break;
+    case NGHTTP2_HEADERS:
+        hp->state = H2_RECV_HEADERS;
+        break;
+    case NGHTTP2_SETTINGS:
+        hp->state = H2_RECV_SETTINGS;
+        break;
+    case NGHTTP2_PING:
+        hp->state = H2_RECV_PING;
+        break;
+    case NGHTTP2_RST_STREAM:
+    case NGHTTP2_WINDOW_UPDATE:
+        // ignore
+        return 0;
+    default:
+        break;
+    }
+    if (hp->state == H2_RECV_HEADERS && hp->parsed->http_cb)
+    {
+        hp->parsed->http_cb(hp->parsed, HP_HEADERS_COMPLETE, NULL, 0);
+    }
+    if (frame->hd.stream_id >= hp->stream_id)
+    {
+        hp->stream_id = frame->hd.stream_id;
+        hp->stream_closed = 0;
+        if (frame->hd.flags & NGHTTP2_FLAG_END_STREAM)
+        {
+            printd("on_stream_closed stream_id=%d\n", hp->stream_id);
+            hp->stream_closed = 1;
+            hp->frame_type_when_stream_closed = frame->hd.type;
+            if (hp->state == H2_RECV_HEADERS || hp->state == H2_RECV_DATA)
+            {
+                if (hp->parsed->http_cb)
+                {
+                    hp->parsed->http_cb(hp->parsed, HP_MESSAGE_COMPLETE, NULL, 0);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
 
 static inline void upStream(tunnel_t *self, context_t *c)
 {
