4adb37979ffaa3ba2b0096c99af63174e7ecde4a
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-03-16 20:00:33 +0300

    refactor

--- core/static_tunnels.c
+++ core/static_tunnels.c
@@ -2,6 +2,17 @@
 #include "library_loader.h"
 #include "loggers/core_logger.h"
 
+
+#define USING(x)                                                                       \
+    do                                                                                 \
+    {                                                                                  \
+        hash_t h = calcHashLen(#x, strlen(#x));                                        \
+        registerStaticLib((tunnel_lib_t){h,                                            \
+                                         new##x, api##x, destroy##x, getMetadata##x}); \
+        LOGD("Imported static tunnel lib%s.a  hash:%lx", #x, h);                       \
+    } while (0)
+
+
 #ifdef INCLUDE_TCP_LISTENER
 #include "tunnels/adapters/tcp_listener/tcp_listener.h"
 #endif
@@ -46,15 +57,6 @@
 #include "tunnels/client/protobuf/protobuf_client.h"
 #endif
 
-
-#define USING(x)                                                                       \
-    do                                                                                 \
-    {                                                                                  \
-        hash_t h = calcHashLen(#x, strlen(#x));                                        \
-        registerStaticLib((tunnel_lib_t){h,                                            \
-                                         new##x, api##x, destroy##x, getMetadata##x}); \
-        LOGD("Imported static tunnel lib%s.a  hash:%lx", #x, h);                       \
-    } while (0)
 
 void loadStaticTunnelsIntoCore()
 {
