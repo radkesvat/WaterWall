37d7bb4881a97aefae6e3aa7d9c23f6e354662ae
radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-04-24 22:51:03 +0000

    fix bug

--- ww/buffer_stream.c
+++ ww/buffer_stream.c
@@ -13,11 +13,12 @@ buffer_stream_t *newBufferStream(struct buffer_pool_s *pool)
 void empytBufferStream(buffer_stream_t *self)
 {
     c_foreach(i, queue, self->q) { reuseBuffer(self->pool, *i.ref); }
+    queue_clear(&self->q);
 }
 
 void destroyBufferStream(buffer_stream_t *self)
 {
-    empytBufferStream(self);
+    c_foreach(i, queue, self->q) { reuseBuffer(self->pool, *i.ref); }
     queue_drop(&self->q);
     free(self);
 }
