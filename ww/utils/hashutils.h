680c035027b419aeb20e5d15f040f115c7b13374
radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-04-28 16:56:13 +0000

    support another hash alg

--- ww/utils/hashutils.h
+++ ww/utils/hashutils.h
@@ -2,7 +2,22 @@
 #include "basic_types.h"
 #include "komihash.h"
 
-#define KOMIHASH_SEED 0
+static inline uint64_t hashFnv1a64(const uint8_t *buf, size_t len)
+{
+    const uint64_t prime = 0x100000001B3;      // pow(2,40) + pow(2,8) + 0xb3
+    uint64_t       hash  = 0xCBF29CE484222325; // seed
+    const uint8_t *end   = buf + len;
+    while (buf < end)
+    {
+        hash = (*buf++ ^ hash) * prime;
+    }
+    return hash;
+}
 
-#define CALC_HASH_PRIMITIVE(x)  komihash((x), sizeof((x)), KOMIHASH_SEED)
-#define CALC_HASH_BYTES(x, len) komihash((x), len, KOMIHASH_SEED)
+#define CALC_HASH_PRIMITIVE(x)  hashFnv1a64((const uint8_t *) &(x), sizeof((x)))
+#define CALC_HASH_BYTES(x, len) hashFnv1a64((const uint8_t *) (x), (len))
+
+// #define KOMIHASH_SEED 0
+
+// #define CALC_HASH_PRIMITIVE(x)  komihash((x), sizeof((x)), KOMIHASH_SEED)
+// #define CALC_HASH_BYTES(x, len) komihash((x), len, KOMIHASH_SEED)
