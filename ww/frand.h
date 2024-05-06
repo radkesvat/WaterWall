1c7a3fb8a23fc31e00c0efa8be105c83a3744291
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-05-06 22:12:34 +0000

    reformat

--- ww/frand.h
+++ ww/frand.h
@@ -2,18 +2,26 @@
 #include "ww.h"
 
 /*
-    about 2 times faster than default rand()
-    not a secure random!
-    not a secure random!
-    not a secure random!
+    Generic random implementations, to be faster or provider other features...
+
+    "frand_seed" is initialized by ww (generally time()) the seed is a global ww variable
+
+    todo (seed update) the seed is 1 time initialized, i thinks its good if we update it in every idle cycles
 */
 
-// seed initialized by ww (generally time()) the seed is a global ww variable
 
-// Compute a pseudorandom integer.
-// Output value in range [0, 32767]
-// accessing this function from multiple threads will ofcourse corrupt the seed
-// but thats actually good, the output can be true random !
+/*
+
+Compute a pseudorandom integer.
+Output value in range [0, 32767]
+
+accessing this function from multiple threads will ofcourse corrupt the seed, since its not atomic
+but that is actually good, the output can be true random !
+
+about 2 times faster than default rand()
+not a secure random!
+
+*/
 inline uint32_t fastRand(void)
 {
     frand_seed = (214013 * frand_seed + 2531011);
