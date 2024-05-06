11152d7bb8c383f7101fe5f7fab0b05744dc7cc3
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-05-06 01:37:48 +0000

    fix comments messed up

--- tunnels/adapters/connector/udp/udp_connector.h
+++ tunnels/adapters/connector/udp/udp_connector.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "api.h"
 
-// con <-----\                    /----->  Resolve=>  Udp Associate
+// con <-----\                    /----->   Resolve=>  Udp Associate
 // con <------>   UdpConnector    <------>  Resolve=>  Udp Associate
-// con <-----/                    \----->  Resolve=>  Udp Associate
+// con <-----/                    \----->   Resolve=>  Udp Associate
 //
 
 tunnel_t *        newUdpConnector(node_instance_context_t *instance_info);
