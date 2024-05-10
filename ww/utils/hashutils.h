d1dc2421d19688208cfe5b4b4386bb280abfb4a2
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-05-10 11:11:42 +0000

    more comments

--- ww/utils/hashutils.h
+++ ww/utils/hashutils.h
@@ -2,20 +2,27 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/*
+    Some general and performant 64bit hash calculaction functions
+
+
+*/
+
+
 // todo (benchmark) komihash vs Flower–Noll–Vo
 // todo (siphash) add sip hash
 
-// static inline uint64_t hashFnv1a64(const uint8_t *buf, size_t len)
-// {
-//     const uint64_t prime = 0x100000001B3;      // pow(2,40) + pow(2,8) + 0xb3
-//     uint64_t       hash  = 0xCBF29CE484222325; // seed
-//     const uint8_t *end   = buf + len;
-//     while (buf < end)
-//     {
-//         hash = (*buf++ ^ hash) * prime;
-//     }
-//     return hash;
-// }
+static inline uint64_t hashFnv1a64(const uint8_t *buf, size_t len)
+{
+    const uint64_t prime = 0x100000001B3;      // pow(2,40) + pow(2,8) + 0xb3
+    uint64_t       hash  = 0xCBF29CE484222325; // seed
+    const uint8_t *end   = buf + len;
+    while (buf < end)
+    {
+        hash = (*buf++ ^ hash) * prime;
+    }
+    return hash;
+}
 
 // #define CALC_HASH_PRIMITIVE(x)  hashFnv1a64((const uint8_t *) &(x), sizeof((x)))
 // #define CALC_HASH_BYTES(x, len) hashFnv1a64((const uint8_t *) (x), (len))
