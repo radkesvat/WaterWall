0ceed719dce4598638a5b1e129620f1ac1b1d265
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-05-15 21:44:00 +0000

    fix return type

--- ww/utils/mathutils.h
+++ ww/utils/mathutils.h
@@ -3,5 +3,5 @@
 #include <sys/types.h>
 #undef max
 #undef min
-static inline size_t min(ssize_t x, ssize_t y) { return (((x) < (y)) ? (x) : (y)); }
-static inline size_t max(ssize_t x, ssize_t y) { return (((x) < (y)) ? (y) : (x)); }
+static inline ssize_t min(ssize_t x, ssize_t y) { return (((x) < (y)) ? (x) : (y)); }
+static inline ssize_t max(ssize_t x, ssize_t y) { return (((x) < (y)) ? (y) : (x)); }
