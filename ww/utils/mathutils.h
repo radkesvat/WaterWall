adef943c248761305780ac30d3fbfa1df5731ec9
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-06-14 13:14:06 +0000

    more useful math functions

--- ww/utils/mathutils.h
+++ ww/utils/mathutils.h
@@ -15,3 +15,14 @@ typedef int64_t    ssize_t;
 #undef min
 static inline ssize_t min(ssize_t x, ssize_t y) { return (((x) < (y)) ? (x) : (y)); }
 static inline ssize_t max(ssize_t x, ssize_t y) { return (((x) < (y)) ? (y) : (x)); }
+
+
+#define ISSIGNED(t) (((t)(-1)) < ((t) 0))
+
+#define UMAXOF(t) (((0x1ULL << ((sizeof(t) * 8ULL) - 1ULL)) - 1ULL) | \
+                    (0xFULL << ((sizeof(t) * 8ULL) - 4ULL)))
+
+#define SMAXOF(t) (((0x1ULL << ((sizeof(t) * 8ULL) - 1ULL)) - 1ULL) | \
+                    (0x7ULL << ((sizeof(t) * 8ULL) - 4ULL)))
+
+#define MAXOF(t) ((unsigned long long) (ISSIGNED(t) ? SMAXOF(t) : UMAXOF(t)))
