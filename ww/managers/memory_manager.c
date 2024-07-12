e0db3b1592155f407341171b0ff2510563c70036
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-07-12 14:02:23 +0000

    replace wof alloc with mimalloc

--- ww/managers/memory_manager.c
+++ ww/managers/memory_manager.c
@@ -1,6 +1,6 @@
 #include "memory_manager.h"
-#include "alloc-engine/wof_allocator.h"
 #include "hmutex.h"
+#include "mimalloc.h"
 
 #include <assert.h>
 #include <stdarg.h>
@@ -10,9 +10,9 @@
 
 struct dedicated_memory_s
 {
-    hhybridmutex_t   mut;
-    wof_allocator_t *wof_state;
-    unsigned int     free_counter;
+    hhybridmutex_t mut;
+    mi_heap_t     *mi_heap;
+    unsigned int   free_counter;
 };
 enum
 {
@@ -47,9 +47,11 @@ static dedicated_memory_t *state;
 
 dedicated_memory_t *createWWMemoryManager(void)
 {
-    assert(state == NULL);
-    state = createWWDedicatedMemory();
-    return state;
+
+    return NULL;
+    // assert(state == NULL);
+    // state = createWWDedicatedMemory();
+    // return state;
 }
 
 dedicated_memory_t *getWWMemoryManager(void)
@@ -59,55 +61,82 @@ dedicated_memory_t *getWWMemoryManager(void)
 
 void setWWMemoryManager(dedicated_memory_t *new_state)
 {
-    assert(state == NULL);
-    state = new_state;
+    (void) new_state;
+    
+    // assert(state == NULL);
+    // state = new_state;
 }
 
 dedicated_memory_t *createWWDedicatedMemory(void)
 {
-    dedicated_memory_t *dm = malloc(sizeof(dedicated_memory_t));
-    *dm                    = (struct dedicated_memory_s) {.free_counter = 0, .wof_state = wof_allocator_new()};
-    hhybridmutex_init(&dm->mut);
-    return dm;
+    return NULL;
+
+    // dedicated_memory_t *dm = malloc(sizeof(dedicated_memory_t));
+    // *dm                    = (struct dedicated_memory_s) {.free_counter = 0, .mi_heap = mi_heap_new()};
+    // hhybridmutex_init(&dm->mut);
+    // return dm;
 }
 
+/*
+
+    Note: mimalloc has its own thred local heaps, makes no sense if we uses dedicated mem and mutex for it.
+
+*/
 void *wwmGlobalMalloc(size_t size)
 {
-    return wwmDedicatedMalloc(state, size);
+    return mi_malloc(size);
 }
 void *wwmGlobalRealloc(void *ptr, size_t size)
 {
-    return wwmDedicatedRealloc(state, ptr, size);
+    return mi_realloc(ptr, size);
 }
 void wwmGlobalFree(void *ptr)
 {
-    wwmDedicatedFree(state, ptr);
+    mi_free(ptr);
 }
+
+
+/*
+
+    Note: temporarily map to default allocators since mimalloc has no api for dedicated memory pools
+
+*/
 
 void *wwmDedicatedMalloc(dedicated_memory_t *dm, size_t size)
 {
-    hhybridmutex_lock(&dm->mut);
-    void *ptr = wof_alloc(dm->wof_state, size);
-    hhybridmutex_unlock(&dm->mut);
-    return ptr;
+    (void) dm;
+
+    return wwmGlobalMalloc(size);
+    // hhybridmutex_lock(&dm->mut);
+    // void *ptr = mi_heap_malloc(dm->mi_heap, size);
+    // hhybridmutex_unlock(&dm->mut);
+    // return ptr;
 }
 void *wwmDedicatedRealloc(dedicated_memory_t *dm, void *ptr, size_t size)
 {
-    hhybridmutex_lock(&dm->mut);
-    void *newptr = wof_realloc(dm->wof_state, ptr, size);
-    hhybridmutex_unlock(&dm->mut);
-    return newptr;
+    (void) dm;
+
+    return wwmGlobalRealloc(ptr,size);
+
+    // hhybridmutex_lock(&dm->mut);
+    // void *newptr = mi_heap_realloc(dm->mi_heap, ptr, size);
+    // hhybridmutex_unlock(&dm->mut);
+    // return newptr;
 }
 void wwmDedicatedFree(dedicated_memory_t *dm, void *ptr)
 {
-    hhybridmutex_lock(&dm->mut);
-    wof_free(dm->wof_state, ptr);
-    if (dm->free_counter++ > kFreeThreShouldCounter)
-    {
-        wof_gc(dm->wof_state);
-        dm->free_counter = 0;
-    }
-    hhybridmutex_unlock(&dm->mut);
+    (void) dm;
+
+    wwmGlobalFree(ptr);
+
+    // hhybridmutex_lock(&dm->mut);
+    // wof_free(dm->mi_heap, ptr);
+    // if (dm->free_counter++ > kFreeThreShouldCounter)
+    // {
+    //     wof_gc(dm->mi_heap);
+    //     dm->free_counter = 0;
+    // }
+    // hhybridmutex_unlock(&dm->mut);
 }
 
 #endif
