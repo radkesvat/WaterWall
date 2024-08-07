3c80578315c026061ccb626e349fab1f15203c0c
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2024-08-07 20:13:53 +0000

    reformat

--- tunnels/adapters/device/tun/tun_device.c
+++ tunnels/adapters/device/tun/tun_device.c
@@ -5,9 +5,12 @@
 #include "utils/sockutils.h"
 #include "ww/devices/tun/tun.h"
 
+#define LOG_PACKET_INFO 1
+
 typedef struct tun_device_state_s
 {
     tun_device_t *tdev;
+    line_t      **thread_lines;
 
     char        *name;
     char        *ip_subnet;
@@ -18,24 +21,46 @@ typedef struct tun_device_state_s
 
 static void upStream(tunnel_t *self, context_t *c)
 {
-    (void) self;
-    (void) c;
+    (void) (self);
+    (void) (c);
+    assert(false);
+
+    if (c->payload)
+    {
+        dropContexPayload(c);
+    }
+    destroyContext(c);
 }
 
 static void downStream(tunnel_t *self, context_t *c)
 {
-    (void) self;
-    (void) c;
+    (void) (self);
+    (void) (c);
+    assert(false);
+
+    if (c->payload)
+    {
+        dropContexPayload(c);
+    }
+    destroyContext(c);
 }
 
-static void onIPPacketReceived(struct tun_device_s *tdev, void *userdata, shift_buffer_t *buf, tid_t tid){
+static void onIPPacketReceived(struct tun_device_s *tdev, void *userdata, shift_buffer_t *buf, tid_t tid)
+{
     (void) tdev;
-    (void) userdata;
+    tunnel_t* self = userdata;
+    tun_device_state_t *state = TSTATE((tunnel_t*)self);
 
-    printIPPacketInfo(tdev->name,rawBuf(buf));
-    
-    reuseBuffer(getWorkerBufferPool(tid), buf);
 
+#if LOG_PACKET_INFO
+    printIPPacketInfo(tdev->name, rawBuf(buf));
+#endif
+
+    // reuseBuffer(getWorkerBufferPool(tid), buf);
+
+    context_t* ctx = newContext(state->thread_lines[tid]);
+    ctx->payload = buf;
+    self->up->upStream(self->up, ctx);
 }
 
 tunnel_t *newTunDevice(node_instance_context_t *instance_info)
@@ -71,12 +96,15 @@ tunnel_t *newTunDevice(node_instance_context_t *instance_info)
     char *subnet_part  = slash + 1;
     state->subnet_mask = atoi(subnet_part);
 
+    state->thread_lines = globalMalloc(sizeof(line_t *) * WORKERS_COUNT);
+    for (unsigned int i = 0; i < WORKERS_COUNT; i++)
+    {
+        state->thread_lines[i] = newLine(i);
+    }
 
     state->tdev = createTunDevice(state->name, false, state, onIPPacketReceived);
-    assignIpToTunDevice(state->tdev, state->ip_present,state->subnet_mask);
+    assignIpToTunDevice(state->tdev, state->ip_present, state->subnet_mask);
     bringTunDeviceUP(state->tdev);
-
-
 
     tunnel_t *t   = newTunnel();
     t->state      = state;
