dfdf97794303adceb461455744d1cdf4eac44f6a
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-02-12 01:46:59 +0330

    fix function name misspell

--- ww/base/wproc.c
+++ ww/base/wproc.c
@@ -99,7 +99,7 @@ bool isAdmin(void)
  * @param fail_msg The error message, unused on non-Windows.
  * @return bool Always returns true.
  */
-bool windows_elevate_privileges(const char *app_name, char *fail_msg)
+bool elevatePrivileges(const char *app_name, char *fail_msg)
 {
     (void) app_name;
     (void) fail_msg;
