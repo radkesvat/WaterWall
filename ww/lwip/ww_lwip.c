f9d3a0dfcddbfbb7403f8ecc8e40366cee458890
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-02-19 02:14:07 +0330

    ad tcp print debug functions and reformat

--- ww/lwip/ww_lwip.c
+++ ww/lwip/ww_lwip.c
@@ -13,27 +13,27 @@ void printIPPacketInfo(const char *prefix, const unsigned char *buffer)
 
     if (version == 4)
     {
-        struct ip_hdr *ip_header = (struct ip_hdr *)buffer;
-        ip4_addr_t src_addr, dst_addr;
+        struct ip_hdr *ip_header = (struct ip_hdr *) buffer;
+        ip4_addr_t     src_addr, dst_addr;
         memoryCopy(&src_addr, &ip_header->src, sizeof(ip4_addr_t));
         memoryCopy(&dst_addr, &ip_header->dest, sizeof(ip4_addr_t));
         const char *src_ip = ip4addr_ntoa(&src_addr);
         const char *dst_ip = ip4addr_ntoa(&dst_addr);
-        ret = snprintf(ptr, (size_t)rem, "%s : Packet v4 From %s to %s, Data: ", prefix, src_ip, dst_ip);
+        ret = snprintf(ptr, (size_t) rem, "%s : Packet v4 From %s to %s, Data: ", prefix, src_ip, dst_ip);
     }
     else if (version == 6)
     {
-        struct ip6_hdr *ip6_header = (struct ip6_hdr *)buffer;
-        ip6_addr_t src_addr, dst_addr;
+        struct ip6_hdr *ip6_header = (struct ip6_hdr *) buffer;
+        ip6_addr_t      src_addr, dst_addr;
         memoryCopy(&src_addr, &ip6_header->src, sizeof(ip6_addr_t));
         memoryCopy(&dst_addr, &ip6_header->dest, sizeof(ip6_addr_t));
         const char *src_ip = ip6addr_ntoa(&src_addr);
         const char *dst_ip = ip6addr_ntoa(&dst_addr);
-        ret = snprintf(ptr, (size_t)rem, "%s : Packet v6 From %s to %s, Data: ", prefix, src_ip, dst_ip);
+        ret = snprintf(ptr, (size_t) rem, "%s : Packet v6 From %s to %s, Data: ", prefix, src_ip, dst_ip);
     }
     else
     {
-        ret = snprintf(ptr, (size_t)rem, "%s : Unknown IP version, Data: ", prefix);
+        ret = snprintf(ptr, (size_t) rem, "%s : Unknown IP version, Data: ", prefix);
     }
 
     ptr += ret;
@@ -41,11 +41,77 @@ void printIPPacketInfo(const char *prefix, const unsigned char *buffer)
 
     for (int i = 0; i < 16; i++)
     {
-        ret = snprintf(ptr, (size_t)rem, "%02x ", buffer[i]);
+        ret = snprintf(ptr, (size_t) rem, "%02x ", buffer[i]);
         ptr += ret;
         rem -= ret;
     }
     *ptr = '\0';
 
     LOGD(logbuf);
+}
+
+static void printTcpPacketFlagsInfoNoNewLIne(u8_t flags)
+{
+    if (flags & TCP_FIN)
+    {
+        printDebug("FIN ");
+    }
+    if (flags & TCP_SYN)
+    {
+        printDebug("SYN ");
+    }
+    if (flags & TCP_RST)
+    {
+        printDebug("RST ");
+    }
+    if (flags & TCP_PSH)
+    {
+        printDebug("PSH ");
+    }
+    if (flags & TCP_ACK)
+    {
+        printDebug("ACK ");
+    }
+    if (flags & TCP_URG)
+    {
+        printDebug("URG ");
+    }
+    if (flags & TCP_ECE)
+    {
+        printDebug("ECE ");
+    }
+    if (flags & TCP_CWR)
+    {
+        printDebug("CWR ");
+    }
+}
+
+void printTcpPacketInfo(struct tcp_hdr *tcphdr)
+{
+    printDebug("TCP header:\n");
+    printDebug("+-------------------------------+\n");
+    printDebug("|    %5" U16_F "      |    %5" U16_F "      | (src port, dest port)\n", lwip_ntohs(tcphdr->src),
+               lwip_ntohs(tcphdr->dest));
+    printDebug("+-------------------------------+\n");
+    printDebug("|           %010" U32_F "          | (seq no)\n", lwip_ntohl(tcphdr->seqno));
+    printDebug("+-------------------------------+\n");
+    printDebug("|           %010" U32_F "          | (ack no)\n", lwip_ntohl(tcphdr->ackno));
+    printDebug("+-------------------------------+\n");
+    printDebug("| %2" U16_F " |   |%" U16_F "%" U16_F "%" U16_F "%" U16_F "%" U16_F "%" U16_F "|     %5" U16_F
+               "     | (hdrlen, flags ( ",
+               TCPH_HDRLEN(tcphdr), (u16_t) (TCPH_FLAGS(tcphdr) >> 5 & 1), (u16_t) (TCPH_FLAGS(tcphdr) >> 4 & 1),
+               (u16_t) (TCPH_FLAGS(tcphdr) >> 3 & 1), (u16_t) (TCPH_FLAGS(tcphdr) >> 2 & 1),
+               (u16_t) (TCPH_FLAGS(tcphdr) >> 1 & 1), (u16_t) (TCPH_FLAGS(tcphdr) & 1), lwip_ntohs(tcphdr->wnd));
+    printTcpPacketFlagsInfoNoNewLIne(TCPH_FLAGS(tcphdr));
+    printDebug("), win)\n");
+    printDebug("+-------------------------------+\n");
+    printDebug("|    0x%04" X16_F "     |     %5" U16_F "     | (chksum, urgp)\n", lwip_ntohs(tcphdr->chksum),
+               lwip_ntohs(tcphdr->urgp));
+    printDebug("+-------------------------------+\n");
+}
+
+void printTcpPacketFlagsInfo(u8_t flags)
+{
+    printTcpPacketFlagsInfoNoNewLIne(flags);
+    printDebug("\n");
 }
--- ww/lwip/ww_lwip.h
+++ ww/lwip/ww_lwip.h
@@ -1,83 +1,88 @@
 #pragma once
-#include "wlibc.h"
 
+// Standard and library includes
+#include "wlibc.h"
+#include "lwip/autoip.h"
+#include "lwip/inet_chksum.h"
 #include "lwip/init.h"
 #include "lwip/ip.h"
 #include "lwip/ip4_frag.h"
 #include "lwip/ip_addr.h"
 #include "lwip/pbuf.h"
+#include "lwip/prot/tcp.h"
 #include "lwip/tcpip.h"
-#include "lwip/autoip.h"
 
+// ------------------------------------------------------------------------
+// Type definitions and aliases
+// ------------------------------------------------------------------------
 typedef struct ip_hdr  ip4_hdr_t;
 typedef struct ip6_hdr ip6_hdr_t;
 
-#define NETIF_FLAG_L3TO4 0x80U // our custom flag that indicates that the netif is for the PacketToConnection node
+#define NETIF_FLAG_L3TO4 0x80U // Custom flag for PacketToConnection node
 
-// struct ww_netif_clientdata_first{
-//     uint64_t flags;
-// };
+// ------------------------------------------------------------------------
+// Generic IP Function Macros
+// ------------------------------------------------------------------------
+#define ipAddrSetAny      ip_addr_set_any       // Set IP address to wildcard
+#define ipAddrIsAny       ip_addr_isany         // Check if IP address is wildcard
+#define ipAddrCmp         ip_addr_cmp           // Compare two IP addresses
+#define ipAddrNetcmp      ip_addr_netcmp        // Compare two IP addresses with mask
+#define ipAddrCopyFromIp4 ip_addr_copy_from_ip4  // Copy IPv4 address to generic IP
+#define ipAddrCopyFromIp6 ip_addr_copy_from_ip6  // Copy IPv6 address to generic IP
+#define ipAddrCopy        ip_addr_copy          // Copy a generic IP address
+#define ipAddrIsV4        IP_IS_V4              // Check if IP address is IPv4
+#define ipAddrIsV6        IP_IS_V6              // Check if IP address is IPv6
 
-//
-// ============================= Generic IP Functions =============================
-//
-#define ipAddrSetAny      ip_addr_set_any       /**< Set IP address to the wildcard address (either IPv4 or IPv6) */
-#define ipAddrIsAny       ip_addr_isany         /**< Check if an IP address is the wildcard address */
-#define ipAddrCmp         ip_addr_cmp           /**< Compare two IP addresses (both IPv4 or both IPv6) */
-#define ipAddrNetcmp      ip_addr_netcmp        /**< Compare two IP addresses, considering network mask */
-#define ipAddrCopyFromIp4 ip_addr_copy_from_ip4 /**< Copy an IPv4 address to a generic IP address */
-#define ipAddrCopyFromIp6 ip_addr_copy_from_ip6 /**< Copy an IPv6 address to a generic IP address */
-#define ipAddrCopy        ip_addr_copy          /**< Copy a generic IP address */
-#define ipAddrIsV4        IP_IS_V4              /**< Check if an IP address is IPv4 */
-#define ipAddrIsV6        IP_IS_V6              /**< Check if an IP address is IPv6 */
+// ------------------------------------------------------------------------
+// IPv4 Specific Function Macros
+// ------------------------------------------------------------------------
+#define ip4AddrSetAny ip4_addr_set_any
+#define ip4AddrGetU32 ip4_addr_get_u32
+#define ip4AddrNetcmp ip4_addr_netcmp
+#define ip4AddrCopy   ip4_addr_copy
+#define ip4AddrNetworkToAaddress ip4addr_ntoa
 
-//
-// ============================ IPv4 Specific Functions ============================
-//
-#define ip4AddrSetAny ip4_addr_set_any /**< Set IPv4 address to the wildcard address */
-#define ip4AddrGetU32 ip4_addr_get_u32 /**< Get the IPv4 address as a 32-bit unsigned integer */
-#define ip4AddrNetcmp ip4_addr_netcmp  /**< Compare two IPv4 addresses, considering network mask */
-#define ip4AddrCopy   ip4_addr_copy    /**< Copy an IPv4 address */
+// ------------------------------------------------------------------------
+// IPv6 Specific Function Macros
+// ------------------------------------------------------------------------
+#define ip6AddrSetAny ip6_addr_set_any
+#define ip6AddrNetcmp ip6_addr_netcmp
+#define ip6AddrCopyFromPacket ip6_addr_copy_from_packed
+#define ip6AddrNetworkToAaddress ip6addr_ntoa
 
-//
-// ============================ IPv6 Specific Functions ============================
-//
-#define ip6AddrSetAny ip6_addr_set_any /**< Set IPv6 address to the wildcard address */
-#define ip6AddrNetcmp ip6_addr_netcmp  /**< Compare two IPv6 addresses, considering network mask */
-#define ip6AddrCopyFromPacket                                                                                          \
-    ip6_addr_copy_from_packed /**< Copy an IPv6 address from a packed (network byte order) representation */
+// ------------------------------------------------------------------------
+// TCP/IP Stack Initialization Macro
+// ------------------------------------------------------------------------
+#define tcpipInit tcpip_init
 
-#define ip6AddrNetworkToAaddress ip6addr_ntoa /**< Convert an IPv6 address to a string representation */
-#define ip4AddrNetworkToAaddress ip4addr_ntoa /**< Convert an IPv4 address to a string representation */
+// ------------------------------------------------------------------------
+// Packet Buffer Function Macros
+// ------------------------------------------------------------------------
+#define pbufAlloc pbuf_alloc
 
-//
-// ============================= TCP/IP Initialization =============================
-//
-#define tcpipInit tcpip_init /**< Initialize the TCP/IP stack */
-
-//
-// ============================= Packet Buffer Functions =============================
-//
-#define pbufAlloc pbuf_alloc /**< Allocate a pbuf (packet buffer) */
-
+// ------------------------------------------------------------------------
+// Function Prototypes for Debug and Packet Info
+// ------------------------------------------------------------------------
 void printIPPacketInfo(const char *prefix, const unsigned char *buffer);
+void printTcpPacketInfo(struct tcp_hdr *tcphdr);
+void printTcpPacketFlagsInfo(u8_t flags);
 
-
+// ------------------------------------------------------------------------
+// OS-specific adjustments
+// ------------------------------------------------------------------------
 #ifdef OS_WIN
+    #define LWIP_IPSTAT IP_STATS
+    #undef IP_STATS
 
-#define LWIP_IPSTAT IP_STATS
-#undef IP_STATS
+    #define LWIP_ICMPSTAT ICMP_STATS
+    #undef ICMP_STATS
 
-#define LWIP_ICMPSTAT ICMP_STATS
-#undef ICMP_STATS
+    #define LWIP_TCPSTAT TCP_STATS
+    #undef TCP_STATS
 
-#define LWIP_TCPSTAT TCP_STATS
-#undef TCP_STATS
+    #define LWIP_UDPSTAT UDP_STATS
+    #undef UDP_STATS
 
-#define LWIP_UDPSTAT UDP_STATS
-#undef UDP_STATS
-
-#define LWIP_IP6STAT IP6_STATS
-#undef IP6_STATS
-
+    #define LWIP_IP6STAT IP6_STATS
+    #undef IP6_STATS
 #endif
