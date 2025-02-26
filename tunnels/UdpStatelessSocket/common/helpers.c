78c069d49408c33aeca88e007ab2899ccf101a33
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-02-26 23:21:21 +0330

    small change for getting wid when udp packet received

--- tunnels/UdpStatelessSocket/common/helpers.c
+++ tunnels/UdpStatelessSocket/common/helpers.c
@@ -4,30 +4,32 @@
 
 void udpstatelesssocketOnRecvFrom(wio_t *io, sbuf_t *buf)
 {
-    tunnel_t *t = (tunnel_t *) (weventGetUserdata(io));
+    tunnel_t *t   = (tunnel_t *) (weventGetUserdata(io));
+    wid_t     wid = wloopGetWid(weventGetLoop(io));
+    
     if (UNLIKELY(t == NULL))
     {
         assert(false);
-        bufferpoolReuseBuffer(getWorkerBufferPool(getWID()), buf);
+        bufferpoolReuseBuffer(getWorkerBufferPool(wid), buf);
         return;
     }
 
-    if(wioGetPeerAddrU(io)->sa.sa_family == 0)
+    if (wioGetPeerAddrU(io)->sa.sa_family == 0)
     {
         assert(false);
-        bufferpoolReuseBuffer(getWorkerBufferPool(getWID()), buf);
+        bufferpoolReuseBuffer(getWorkerBufferPool(wid), buf);
         return;
     }
 
-    line_t *line      = tunnelchainGetPacketLine(tunnelGetChain(t), getWID());
+    line_t *line      = tunnelchainGetPacketLine(tunnelGetChain(t), wid);
     line->established = true;
- 
+
     char localaddrstr[SOCKADDR_STRLEN] = {0};
     char peeraddrstr[SOCKADDR_STRLEN]  = {0};
 
     LOGD("UdpStatelessSocket: received %u bytes from [%s] <= [%s]", sbufGetBufLength(buf),
          SOCKADDR_STR(wioGetLocaladdrU(io), localaddrstr), SOCKADDR_STR(wioGetPeerAddrU(io), peeraddrstr));
-   
+
     connectioncontextFromSockAddr(&line->routing_context.src_ctx, wioGetPeerAddrU(io));
 
     lineLock(line);
