8c7fc9d2269cd229f60d39a13d452a56c6ae02c8
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-02-28 02:57:34 +0330

    add debug initiation data, fix miscalculation on size

--- ww/bufio/shiftbuffer.c
+++ ww/bufio/shiftbuffer.c
@@ -28,8 +28,12 @@ sbuf_t *sbufNewWithPadding(uint32_t minimum_capacity, uint16_t pad_left)
         minimum_capacity = (max(kCpuLineCacheSize, minimum_capacity) + kCpuLineCacheSizeMin1) & (~kCpuLineCacheSizeMin1);
     }
 
-    uint32_t real_cap = minimum_capacity + pad_left;
+    uint32_t real_cap = minimum_capacity + pad_left + sizeof(sbuf_t);
     sbuf_t  *b        = memoryAllocate(real_cap + 128);
+
+#ifdef DEBUG
+    memorySet(b, 0x55, real_cap);
+#endif
 
     b->len      = 0;
     b->curpos   = pad_left;
