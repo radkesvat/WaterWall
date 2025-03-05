f273e9c1e41d52194dd0678b8ba7e19de170d7bd
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-03-05 19:27:45 +0330

    fix a incorrect comment details

--- tunnels/TcpConnector/upstream/fin.c
+++ tunnels/TcpConnector/upstream/fin.c
@@ -7,7 +7,7 @@ void tcpconnectorTunnelUpStreamFinish(tunnel_t *t, line_t *l)
     tcpconnector_lstate_t *lstate = lineGetState(l, t);
 
     // This indicates that line is closed. Even if we get the closeCallback
-    // while flushing the queue, no FIN will be sent to upstream
+    // while flushing the queue, no FIN will be sent to downstroam
     weventSetUserData(lstate->io, NULL);
 
     tcpconnectorFlushWriteQueue(lstate);
