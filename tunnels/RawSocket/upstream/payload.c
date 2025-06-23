0dc3957cf56d26d4da17425be51d49562026783f
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-06-23 19:15:01 +0000

    log twicks

--- tunnels/RawSocket/upstream/payload.c
+++ tunnels/RawSocket/upstream/payload.c
@@ -5,7 +5,10 @@
 void rawsocketUpStreamPayload(tunnel_t *t, line_t *l, sbuf_t *buf)
 {
     (void) l;
+    // discard t;
     rawsocket_tstate_t *state = tunnelGetState(t);
+
+    // printIPPacketInfo("RawSocket sending: ", sbufGetRawPtr(buf));
 
     if (! writeToRawDevce(state->raw_device, buf))
     {
