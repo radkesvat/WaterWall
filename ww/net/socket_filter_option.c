6aad22c23d60f5714880d38b4417d4804856763e
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-06-24 14:43:15 +0000

    fix

--- ww/net/socket_filter_option.c
+++ ww/net/socket_filter_option.c
@@ -1,6 +1,7 @@
 #include "socket_filter_option.h"
 
 void socketfilteroptionInit(socket_filter_option_t* sfo){
+    memorySet(sfo,0,sizeof(*sfo));
     sfo->white_list = vec_ipmask_t_with_capacity(8);
     sfo->black_list = vec_ipmask_t_with_capacity(8);
     sfo->balance_group_name = NULL;
