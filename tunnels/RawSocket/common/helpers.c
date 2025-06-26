99059368ad5866482e93f0316ce002d0490c29f2
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-06-26 20:12:13 +0000

    disable some extra logs

--- tunnels/RawSocket/common/helpers.c
+++ tunnels/RawSocket/common/helpers.c
@@ -18,9 +18,11 @@ void rawsocketOnIPPacketReceived(struct capture_device_s *cdev, void *userdata, 
 {
     // packet is correctly filtered based on src/dest ip since we told net filter system
     (void) cdev;
-    tunnel_t           *t  = userdata;
-    // rawsocket_tstate_t *state = tunnelGetState(t);
-    printIPPacketInfo("RawSocket received: ", sbufGetRawPtr(buf));
+    tunnel_t *t = userdata;
+
+    //rawsocket_tstate_t *state = tunnelGetState(t);
+    //printIPPacketInfo("RawSocket received: ", sbufGetRawPtr(buf));
+
     line_t *l = tunnelchainGetPacketLine(t->chain, wid);
     lineLock(l);
     tunnelPrevDownStreamPayload(t, l, buf);
