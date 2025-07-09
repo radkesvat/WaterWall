72c46fa8fb928c500d15f8830db8cd1e754cfcf3
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-07-09 04:48:22 +0000

    some comments for the reader

--- tunnels/IpOverrider/upstream/payload.c
+++ tunnels/IpOverrider/upstream/payload.c
@@ -4,5 +4,7 @@
 
 void ipoverriderUpStreamPayload(tunnel_t *t, line_t *l, sbuf_t *buf)
 {
+    // if this direction is used to change ip adderss, see helpers.h and init.c
+    // otherwise pass through
     tunnelNextUpStreamPayload(t, l, buf);
 }
