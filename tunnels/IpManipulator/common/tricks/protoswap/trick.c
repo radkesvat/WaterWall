2041b0ac723148c22cbd3bad52057b94aad28f8d
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-07-18 14:22:40 +0330

    update to support bidirectional swappnig

--- tunnels/IpManipulator/common/tricks/protoswap/trick.c
+++ tunnels/IpManipulator/common/tricks/protoswap/trick.c
@@ -2,7 +2,6 @@
 
 #include "loggers/network_logger.h"
 
-
 void protoswaptrickUpStreamPayload(tunnel_t *t, line_t *l, sbuf_t *buf)
 {
     ipmanipulator_tstate_t *state    = tunnelGetState(t);
@@ -10,15 +9,32 @@ void protoswaptrickUpStreamPayload(tunnel_t *t, line_t *l, sbuf_t *buf)
 
     if (IPH_V(ipheader) == 4)
     {
-        if (IPH_PROTO(ipheader) == IPPROTO_TCP && state->trick_proto_swap_tcp_number != -1)
+        if (state->trick_proto_swap_tcp_number != -1)
         {
-            IPH_PROTO_SET(ipheader, state->trick_proto_swap_tcp_number);
-            l->recalculate_checksum = true;
+            if (IPH_PROTO(ipheader) == IPPROTO_TCP)
+            {
+                IPH_PROTO_SET(ipheader, state->trick_proto_swap_tcp_number);
+                l->recalculate_checksum = true;
+            }
+            else if (IPH_PROTO(ipheader) == state->trick_proto_swap_tcp_number)
+            {
+                IPH_PROTO_SET(ipheader, IPPROTO_TCP);
+                l->recalculate_checksum = true;
+            }
         }
-        else if (IPH_PROTO(ipheader) == IPPROTO_UDP && state->trick_proto_swap_udp_number != -1)
+
+        if (state->trick_proto_swap_udp_number != -1)
         {
-            IPH_PROTO_SET(ipheader, state->trick_proto_swap_udp_number);
-            l->recalculate_checksum = true;
+            if (IPH_PROTO(ipheader) == IPPROTO_UDP)
+            {
+                IPH_PROTO_SET(ipheader, state->trick_proto_swap_udp_number);
+                l->recalculate_checksum = true;
+            }
+            else if (IPH_PROTO(ipheader) == state->trick_proto_swap_udp_number)
+            {
+                IPH_PROTO_SET(ipheader, IPPROTO_UDP);
+                l->recalculate_checksum = true;
+            }
         }
     }
 
@@ -32,15 +48,32 @@ void protoswaptrickDownStreamPayload(tunnel_t *t, line_t *l, sbuf_t *buf)
 
     if (IPH_V(ipheader) == 4)
     {
-        if (state->trick_proto_swap_tcp_number != -1 && IPH_PROTO(ipheader) == state->trick_proto_swap_tcp_number)
+        if (state->trick_proto_swap_tcp_number != -1)
         {
-            IPH_PROTO_SET(ipheader, IPPROTO_TCP);
-            l->recalculate_checksum = true;
+            if (IPH_PROTO(ipheader) == IPPROTO_TCP)
+            {
+                IPH_PROTO_SET(ipheader, state->trick_proto_swap_tcp_number);
+                l->recalculate_checksum = true;
+            }
+            else if (IPH_PROTO(ipheader) == state->trick_proto_swap_tcp_number)
+            {
+                IPH_PROTO_SET(ipheader, IPPROTO_TCP);
+                l->recalculate_checksum = true;
+            }
         }
-        else if (state->trick_proto_swap_udp_number != -1 && IPH_PROTO(ipheader) == state->trick_proto_swap_udp_number)
+
+        if (state->trick_proto_swap_udp_number != -1)
         {
-            IPH_PROTO_SET(ipheader, IPPROTO_UDP);
-            l->recalculate_checksum = true;
+            if (IPH_PROTO(ipheader) == IPPROTO_UDP)
+            {
+                IPH_PROTO_SET(ipheader, state->trick_proto_swap_udp_number);
+                l->recalculate_checksum = true;
+            }
+            else if (IPH_PROTO(ipheader) == state->trick_proto_swap_udp_number)
+            {
+                IPH_PROTO_SET(ipheader, IPPROTO_UDP);
+                l->recalculate_checksum = true;
+            }
         }
     }
 
