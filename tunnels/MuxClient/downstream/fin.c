e19c76022f0cf2dc6c56c0d29be487500802bbae
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-07-27 01:31:08 +0000

    fix: should clear the worker last mux connection when closing

--- tunnels/MuxClient/downstream/fin.c
+++ tunnels/MuxClient/downstream/fin.c
@@ -5,12 +5,19 @@
 void muxclientTunnelDownStreamFinish(tunnel_t *t, line_t *parent_l)
 {
     muxclient_lstate_t *parent_ls = lineGetState(parent_l, t);
+    muxclient_lstate_t *child_ls  = parent_ls->child_next;
 
-    muxclient_lstate_t *child_ls = parent_ls->child_next;
+    muxclient_tstate_t *ts  = tunnelGetState(t);
+    wid_t               wid = lineGetWID(parent_l);
+    if (ts->unsatisfied_lines[wid] == parent_l)
+    {
+        ts->unsatisfied_lines[wid] = NULL;
+    }
+
     while (child_ls)
     {
-        muxclient_lstate_t *temp = child_ls->child_next;
-        line_t *child_l = child_ls->l;
+        muxclient_lstate_t *temp    = child_ls->child_next;
+        line_t             *child_l = child_ls->l;
         muxclientLeaveConnection(child_ls);
         muxclientLinestateDestroy(child_ls);
         tunnelPrevDownStreamFinish(t, child_l);
