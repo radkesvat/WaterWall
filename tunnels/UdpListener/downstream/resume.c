c493a72831120890cd59915e4e00ef1a6ef2f972
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-08-03 23:32:33 +0330

    bug fix in udp connector

--- tunnels/UdpListener/downstream/resume.c
+++ tunnels/UdpListener/downstream/resume.c
@@ -6,8 +6,8 @@ void udplistenerTunnelDownStreamResume(tunnel_t *t, line_t *l)
 {
     udplistener_lstate_t *ls = lineGetState(l, t);
 
-    if (! ls->read_paused)
+    if (ls->read_paused)
     {
-        ls->read_paused = true;
+        ls->read_paused = false;
     }
 }
