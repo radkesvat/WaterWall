7f07fe8652bec0568aa58d3deee94d85ec30e12b
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-08-04 08:11:39 +0330

    use object return type instead of pointer

--- ww/bufio/context_queue.c
+++ ww/bufio/context_queue.c
@@ -3,38 +3,24 @@
 #include "stc/common.h"
 #include "tunnel.h"
 
-#define i_type queue_t
-#define i_key context_t *
-#include "stc/deque.h"
 
 enum
 {
     kQCap = 8
 };
 
-struct context_queue_s
-{
-    queue_t          q;
-};
 
-/*
-    Creates a new context queue.
-    @return A pointer to the newly created context queue, or NULL on failure.
-*/
-context_queue_t *contextqueueCreate(void)
+context_queue_t contextqueueCreate(void)
 {
-    context_queue_t *cb = memoryAllocate(sizeof(context_queue_t));
-    cb->q               = queue_t_with_capacity(kQCap);
-    return cb;
+
+    context_queue_t cq;
+    cq.q = ww_context_queue_t_with_capacity(kQCap);
+    return cq;
 }
 
-/*
-    Destroys a context queue, freeing all its resources.
-    @param self A pointer to the context queue to destroy.
-*/
 void contextqueueDestory(context_queue_t *self)
 {
-    c_foreach(i, queue_t, self->q)
+    c_foreach(i, ww_context_queue_t, self->q)
     {
         if ((*i.ref)->payload != NULL)
         {
@@ -43,37 +29,21 @@ void contextqueueDestory(context_queue_t *self)
         contextDestroy((*i.ref));
     }
 
-    queue_t_drop(&self->q);
-    memoryFree(self);
+    ww_context_queue_t_drop(&self->q);
 }
 
-/*
-    Pushes a context onto the back of the queue.
-    @param self A pointer to the context queue.
-    @param context A pointer to the context to push onto the queue.
-*/
 void contextqueuePush(context_queue_t *self, context_t *context)
 {
-    queue_t_push_back(&self->q, context);
+    ww_context_queue_t_push_back(&self->q, context);
 }
 
-/*
-    Pops a context from the front of the queue.
-    @param self A pointer to the context queue.
-    @return A pointer to the context that was popped from the queue, or NULL if the queue is empty.
-*/
 context_t *contextqueuePop(context_queue_t *self)
 {
-    context_t *context = queue_t_pull_front(&self->q);
+    context_t *context = ww_context_queue_t_pull_front(&self->q);
     return context;
 }
 
-/*
-    Gets the number of contexts in the queue.
-    @param self A pointer to the context queue.
-    @return The number of contexts in the queue.
-*/
 size_t contextqueueLen(context_queue_t *self)
 {
-    return (size_t) (queue_t_size(&self->q));
+    return (size_t) (ww_context_queue_t_size(&self->q));
 }
--- ww/bufio/context_queue.h
+++ ww/bufio/context_queue.h
@@ -1,15 +1,21 @@
 #pragma once
 
-#include "tunnel.h"
 #include "context.h"
+#include "tunnel.h"
 #include "wlibc.h"
+
+#define i_type ww_context_queue_t
+#define i_key  context_t *
+#include "stc/deque.h"
 
 /*
     a queue for context type, this helps storing it and
     later using it, its a very simple queue
 */
-
-typedef struct context_queue_s context_queue_t;
+typedef struct context_queue_s
+{
+    ww_context_queue_t q;
+} context_queue_t;
 
 /*
     Represents a queue of context_t pointers.
@@ -20,7 +26,7 @@ struct context_queue_s;
     Creates a new context queue.
     @return A pointer to the newly created context queue, or NULL on failure.
 */
-context_queue_t *contextqueueCreate(void);
+context_queue_t contextqueueCreate(void);
 
 /*
     Destroys a context queue, freeing all its resources.
