4914fbb7f997620fc609f70a8d92ee81c3ccca0b
Radkesvat <134321679+radkesvat@users.noreply.github.com>
2025-08-23 23:47:01 +0000

    add WW_IS_64_BIT

--- ww/libc/wdef.h
+++ ww/libc/wdef.h
@@ -346,7 +346,7 @@ typedef uint16_t  u16_t;
 typedef int16_t   s16_t;
 typedef uint32_t  u32_t;
 typedef int32_t   s32_t;
-#if LWIP_HAVE_INT64
+#if WW_IS_64_BIT
 typedef uint64_t  u64_t;
 typedef int64_t   s64_t;
 #endif
