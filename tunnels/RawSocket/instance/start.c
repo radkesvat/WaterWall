e23f487922e22eb2d4bf6ae1c711289b2212ceb1
radkesvat <134321679+radkesvat@users.noreply.github.com>
2026-05-25 05:55:34 +0000

    RawSocket: move startup logic from prepair.c to start.c

--- tunnels/RawSocket/instance/start.c
+++ tunnels/RawSocket/instance/start.c
@@ -4,6 +4,32 @@
 
 void rawsocketOnStart(tunnel_t *t)
 {
-    discard t;
+    rawsocket_tstate_t *state = tunnelGetState(t);
+
+    if (nodeIsLastInChain(t->node))
+    {
+        state->WriteReceivedPacket = t->prev->fnPayloadD;
+        state->write_tunnel = t->prev;
+    }
+    else
+    {
+        state->WriteReceivedPacket = t->next->fnPayloadU;
+        state->write_tunnel = t->next;
+    }
+
+    state->capture_device = caputredeviceCreate(state->capture_device_name, state->capture_ranges,
+                                                state->capture_range_count, t, rawsocketOnIPPacketReceived);
+
+    if (state->capture_device == NULL)
+    {
+        LOGF("CaptureDevice: could not create device");
+        terminateProgram(1);
+    }
+
+    // we are not going to read, so pass read call back as null therfore no buffers for read will be allocated
+    state->raw_device = rawdeviceCreate(state->raw_device_name, state->firewall_mark, t);
+
+    caputredeviceBringUp(state->capture_device);
+    rawdeviceBringUp(state->raw_device);
 }
 
