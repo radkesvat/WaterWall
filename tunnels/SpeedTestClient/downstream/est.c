d9031723c8cff59c774ed8a97e46a7b154bc824c
radkesvat <134321679+radkesvat@users.noreply.github.com>
2026-06-10 20:35:42 +0000

    Log speedtest pacing target

--- tunnels/SpeedTestClient/downstream/est.c
+++ tunnels/SpeedTestClient/downstream/est.c
@@ -6,6 +6,7 @@ void speedtestclientTunnelDownStreamEst(tunnel_t *t, line_t *l)
 {
     speedtestclient_tstate_t *state = tunnelGetState(t);
     speedtestclient_lstate_t *ls = lineGetState(l, t);
+    char                      target_buf[64];
 
     if (! lineIsEstablished(l))
     {
@@ -13,12 +14,22 @@ void speedtestclientTunnelDownStreamEst(tunnel_t *t, line_t *l)
     }
 
     ls->est_received = true;
-    LOGI("SpeedTestClient: stream %u established (%s, %s%s%s)",
+    if (state->target_bandwidth_bps == 0)
+    {
+        stringNPrintf(target_buf, sizeof(target_buf), "unlimited");
+    }
+    else
+    {
+        speedtestclientFormatBitrate((double) state->target_bandwidth_bps, target_buf, sizeof(target_buf));
+    }
+
+    LOGI("SpeedTestClient: stream %u established (%s, %s%s%s, target %s)",
          (unsigned int) ls->stream_id,
          state->mode == kSpeedTestClientModeUdp ? "udp" : "tcp",
          state->upload ? "upload" : "",
          (state->upload && state->download) ? "+" : "",
-         state->download ? "download" : "");
+         state->download ? "download" : "",
+         target_buf);
 
     if (state->upload)
     {
@@ -30,4 +41,3 @@ void speedtestclientTunnelDownStreamEst(tunnel_t *t, line_t *l)
     }
     speedtestclientScheduleReport(t, l, ls);
 }
-
